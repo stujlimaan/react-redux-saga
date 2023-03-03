d4bbb1e17bf3c78e972b54e7284cad9bc7d07e5f
stujliman <stujliman@mindfiresolutions.com>
2023-03-03 17:52:14 +0530

    make productlist, action,reducers for product, fetch api call in action

--- ecom-proj/src/redux/Store.js
+++ ecom-proj/src/redux/Store.js
@@ -1,6 +1,10 @@
-import {createStore} from 'redux'
+// import {createStore} from 'redux'
+
+//using redux toolkit to create store
+import {configureStore} from "@reduxjs/toolkit"
 import { rootReducer } from './rootReducer'
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
 
+const store = configureStore({reducer:rootReducer})
 export default store
